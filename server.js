13cd54a3c9c1991e16e6c7ceaa8f6d46e0af45fc
alyssa-6175 <alyssa.pan@hotmail.com>
2026-02-06 00:57:49 -0800

    add typing...

---
+++ server.js
@@ -0,0 +1,28 @@
+
+// server.js
+const WebSocket = require("ws");
+const http = require("http");
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+app.use(express.static("public"));
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+wss.on("connection", (socket) => {
+    console.log("Client connected");
+    socket.on("message", (data) => {
+        // Broadcast to everyone
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(data.toString());
+            }
+        });
+    });
+});
+server.listen(PORT, () => {
+    const url =
+        process.env.RENDER_EXTERNAL_URL ||
+        `http://localhost:${PORT}`;
+    console.log(`Server running at ${url}`);
+});
